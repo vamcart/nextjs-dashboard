2c1f512b71ca016dd4886e66ca6153a70a0130c1
VamCart <vamcart@gmail.com>
2025-01-19 16:14:05 +0300

    правки

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,12 +30,13 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+// Fetch the last 5 invoices, sorted by date
+const data = await sql<LatestInvoiceRaw>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 
 export default async function LatestInvoices({
-  //latestInvoices,
+  latestInvoices,
 }: {
-  //latestInvoices: LatestInvoice[];
+  latestInvoices: LatestInvoice[];
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -14,9 +14,7 @@ export default async function LatestInvoices({
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: Uncomment this code in Chapter 7 */}
-
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -53,7 +51,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
