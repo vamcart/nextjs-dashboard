1b682d31f3bc2d7501cb33728cf4d458d0f32589
VamCart <vamcart@gmail.com>
2025-01-19 17:25:27 +0300

    добавлена skeleton анимация для всех блоков

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -29,6 +29,10 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+
+    //Задержка 4 секунды что б увидеть skeleton анимацию
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -49,6 +53,10 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+
+    //Задержка 4 секунды что б увидеть skeleton анимацию
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
