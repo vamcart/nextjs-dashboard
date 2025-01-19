c92cc7d27ba0eab97205341b921880f70ef10932
VamCart <vamcart@gmail.com>
2025-01-19 17:11:29 +0300

    skeleton анимация при загрузке dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,16 +11,15 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
+    // We artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+ 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
+ 
+    console.log('Data fetch completed after 3 seconds.');
+ 
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
