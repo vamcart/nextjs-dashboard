93f2c0b6077ddacd7b8fc62bdd5851fed3ff69eb
VamCart <vamcart@gmail.com>
2025-01-20 12:18:26 +0300

    тест деплоя

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices } from '@/app/lib/data';
 
 export default async function LatestInvoices() { // Make component async, remove the props
-  const latestInvoices = await fetchLatestInvoices(); // Fetch data inside the component
+const latestInvoices = await fetchLatestInvoices(); // Fetch data inside the component
  
   return (
     <div className="flex w-full flex-col md:col-span-4">
