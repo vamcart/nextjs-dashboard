01af51380e0ae484b81366d38e5c853e8578eb10
VamCart <vamcart@gmail.com>
2025-01-20 12:24:33 +0300

    тест

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
     authenticate,
     undefined,
   );
- 
+
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
