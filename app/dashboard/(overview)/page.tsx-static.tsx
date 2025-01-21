445b879cad7e94f47c79ca3c4b486b30431a3547
VamCart <vamcart@gmail.com>
2025-01-21 16:13:44 +0300

    новый файл - пример static layout

--- app/dashboard/(overview)/page.tsx-static.tsx
+++ app/dashboard/(overview)/page.tsx-static.tsx
@@ -10,24 +10,15 @@ export const metadata: Metadata = {
   title: 'Home',
 };
 
-export default async function Page(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
-}) {
+export default async function Page() {
 
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense key={query + currentPage} fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
