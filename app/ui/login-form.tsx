c4a3cbc3ccb01f4ea081cffa79e863940e336de7
VamCart <vamcart@gmail.com>
2025-01-22 16:29:51 +0300

    Корректировка логина

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,5 +1,5 @@
 'use client';
- 
+
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -10,8 +10,11 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
- 
-export default function LoginForm({ redirectTo }: { redirectTo: string }) {
+import { useSearchParams } from 'next/navigation';
+
+export default function LoginForm() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
     undefined,
@@ -64,9 +67,9 @@ export default function LoginForm({ redirectTo }: { redirectTo: string }) {
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            <input type="hidden" name="redirectTo" value={redirectTo} />
           </div>
         </div>
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
         <Button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
