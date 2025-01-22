68ec561f0d2ecb0da35ef425b04d6cb01843976d
VamCart <vamcart@gmail.com>
2025-01-22 18:26:00 +0300

    Корректировка вывода данных, вынесен код из компонента

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -233,3 +233,22 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customer table.');
   }
 }
+
+export async function fetchPokemons() {
+    const data = await fetch(
+      // We intentionally delay the response to simulate a slow data
+      // request that would benefit from streaming
+      `https://app-router-api.vercel.app/api/productss`,
+      {
+        // We intentionally disable Next.js Cache to better demo
+        // streaming
+        cache: 'no-store',
+      }
+    );
+  
+    if (!data.ok) {
+      throw new Error(response.statusText);
+    }
+
+  return data.json()
+}
