1efcfa888c7dc7190c1b23eba49e85fdae38f2e1
VamCart <vamcart@gmail.com>
2025-01-23 20:02:20 +0300

    Корректировка вывода картинок на странице  RTK Query

--- app/ui/pokemons/pokemon-info.tsx
+++ app/ui/pokemons/pokemon-info.tsx
@@ -1,9 +1,10 @@
 'use client';
+import Image from 'next/image';
 import { useGetPokemonByNameQuery } from '@/app/services/pokemon'
 
 export default function PokemonInfo() {
   // Using a query hook automatically fetches data and returns query values
-  const { data, error, isLoading, isFetching, isSuccess, isError } = useGetPokemonByNameQuery('bulbasaur')
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
   // Individual hooks are also accessible under the generated endpoints:
   // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
 
@@ -16,7 +17,13 @@ export default function PokemonInfo() {
       ) : data ? (
         <>
           <h3>{data.species.name}</h3>
-          <img src={data.sprites.front_shiny} alt={data.species.name} />
+              <Image
+                src={data.sprites.front_shiny}
+                className="mr-2 rounded-full"
+                width={28}
+                height={28}
+                alt={`${data.species.name}'s profile picture`}
+              />          
         </>
       ) : null}
     </div>
--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/dashboard/rtk-query/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
